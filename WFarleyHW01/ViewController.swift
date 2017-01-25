e4e2224a8a5f17396874213675429ffba86fd254
William Farley <williamfarley@Williams-MacBook-Pro.local>
2017-01-25 15:06:42 -0500

    Initial Commit

---
+++ WFarleyHW01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WFarleyHW01
+//
+//  Created by William Farley on 1/25/17.
+//  Copyright © 2017 William Farley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
